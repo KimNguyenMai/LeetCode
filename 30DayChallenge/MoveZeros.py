7f14cfaf6c8a49958423cf56212b6837d9d6993f
Kim <maikim200295@gmail.com>
2020-04-05 13:24:54 -0700

    30 Day challenge, day 4: Move Zeros

---
+++ 30DayChallenge/MoveZeros.py
@@ -0,0 +1,15 @@
+#Solution 1:
+for i in nums:
+            if i == 0: 
+                nums.remove(i)
+                nums.append(0)
+                
+#Solution 2:       
+temp = []
+for i in nums:
+    if i == 0: 
+        temp.append(i)
+        nums.pop(nums[i])
+nums += temp
+
+#Solution 3: 2 pointer
