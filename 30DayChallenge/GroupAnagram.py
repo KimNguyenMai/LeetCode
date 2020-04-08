e9d86681b48ed5ff14ce41745393b211d102a930
Kim <maikim200295@gmail.com>
2020-04-07 22:35:53 -0700

    30 challenge: Day 7 - week 1: Counting Elements

---
+++ 30DayChallenge/GroupAnagram.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:                
+        ans = {}
+        for i in strs:
+            key = ''.join(sorted(i))
+            if key not in ans:
+                temp = [i]
+                ans[key] = temp
+            else:
+                ans[key].append(i)
+        return ans.values()
