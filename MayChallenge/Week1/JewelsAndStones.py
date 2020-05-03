64f7add3649efeb5869dd1a12517dd3db70b45bf
Kim Nguyen <kimnguyen@Kims-MacBook-Pro.local>
2020-05-02 22:21:18 -0700

    May Challenge: Jewels and Stones.py

---
+++ MayChallenge/Week1/JewelsAndStones.py
@@ -0,0 +1,23 @@
+#771. 
+
+#Solution1: faster than 99%
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        counter = 0
+        
+        for s in S:
+            if s in J: counter += 1
+                    
+        return counter
+                    
+#Solution2: faster than 69%
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        counter = 0
+        
+        for j in J:
+            for s in S:
+                if s in j: counter += 1
+                    
+        return counter
+                    
