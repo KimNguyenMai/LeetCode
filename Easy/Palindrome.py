bbe7c9b70a83ad1b011e3a36d1e72e8287ec5598
KimNguyenMai <maikim200295@gmail.com>
2020-08-20 22:02:06 -0700

    Palindrome.py

---
+++ Easy/Palindrome.py
@@ -0,0 +1,10 @@
+#         first = 0
+#         last = len(str(x)) - 1
+        
+#         while first >= last:
+#             if str(x)[first] != str(x)[last]:
+#                 return False
+#             else:
+#                 first += 1
+#                 last -= 1
+#                 return True
